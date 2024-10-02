4c4844f42635013cf056e83506145eaf883e42a0
Nikhil <71930916+dishapatel010@users.noreply.github.com>
2024-10-03 00:12:23 +0530

    okie

---
+++ build/precache-manifest.cb85c835f71074987a0ba82a59791a73.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "644df07a9f5fcee6df94",
+    "url": "/static/css/main.eb272938.chunk.css"
+  },
+  {
+    "revision": "644df07a9f5fcee6df94",
+    "url": "/static/js/main.ada8e0fc.chunk.js"
+  },
+  {
+    "revision": "1bd90955e820c97eaaee",
+    "url": "/static/js/runtime~main.632a559e.js"
+  },
+  {
+    "revision": "796466b1c55423b1009a",
+    "url": "/static/js/2.c732ac7a.chunk.js"
+  },
+  {
+    "revision": "3f0dc9c512cfed730230",
+    "url": "/static/js/3.984a5bf2.chunk.js"
+  },
+  {
+    "revision": "dd1175f40956478d928808d244ab7c4b",
+    "url": "/static/media/return.dd1175f4.png"
+  },
+  {
+    "revision": "b6a8fecfdbe74d8460b480b9b7fc0523",
+    "url": "/static/media/lang.b6a8fecf.png"
+  },
+  {
+    "revision": "3f37550d6aa7dad5e5be01d0c03a51d5",
+    "url": "/static/media/mmR.3f37550d.png"
+  },
+  {
+    "revision": "2e4a9e6c4501596d7809cedca07bdf5e",
+    "url": "/static/media/about-us.2e4a9e6c.png"
+  },
+  {
+    "revision": "24e9e417e2f04293280c1dee2d7e49a5",
+    "url": "/static/media/eye-icon.24e9e417.png"
+  },
+  {
+    "revision": "c227e3be4aa7f98976c9813908f7ecd7",
+    "url": "/index.html"
+  }
+];
